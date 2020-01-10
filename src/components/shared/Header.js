d3ec0fa8861a60ed10b0a770c790a9638949c6af
tomkarho <tommi@tomkarho.com>
2020-01-10 14:50:00 +0200

    Shared components
    - separate header component to host navigation links

---
+++ src/components/shared/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+
+function Header() {
+    return (
+        <nav>
+            <NavLink exact to='/'>Home</NavLink>
+            <NavLink to='/about'>About</NavLink>
+        </nav>
+    );
+}
+
+export default Header;
